18554823a64d08e227461a7690581959e8d45d4e
surnan <suresh_nandlall@outlook.com>
2023-10-26 14:43:44 -0400

    Complete: 02-Translation

--- aa01-additional-practice/problems/02-translation-training.js
+++ aa01-additional-practice/problems/02-translation-training.js
@@ -19,12 +19,56 @@
 
     Hint: Use helper methods.
 */
+console.log(translationTraining("What are you doing")); // prints "Wht ar yu dong"
+console.log(translationTraining("This is tedious")); // prints "Ths s tdious"
+console.log(translationTraining("Programming is fun")); // prints "Prgramming s fn"
+console.log(translationTraining("My food is getting cold")) // prints "My fod s gettng cld"
 
-function translationTraining(sentence) {
-    // Your code here 
+function getVowelIndex(front, word){
+    const vowels = 'aeiouAEIOU';
+
+    if (front){
+        for (let i=0; i<word.length; i++){
+            if (vowels.includes(word[i])){
+                return i;
+            }
+        }
+    } else {
+        for (let i=word.length-1; i>=0; i--){
+            if (vowels.includes(word[i])){
+                return i;
+            }
+        }
+    }
+    return -1;  // No vowels found in word
 }
 
-// Your code here 
+function spliceStringCharacter(index, word){
+
+    if (index < 0) {return word}
+
+    let front = word.slice(0,index);
+    let back = word.slice(index+1);
+    return front+back;
+}
+
+
+function translationTraining(sentence) {
+
+    let words = sentence.split(' ');
+    let answer= [];
+    let firstVowel = true;
+
+    for (const word of words){
+        let index = getVowelIndex(firstVowel, word);
+        let newWord = spliceStringCharacter(index, word);
+        answer.push(newWord);
+        firstVowel = !firstVowel;
+    }
+    console.log('answer: ', answer.join(' '));
+    return answer.join(' ');
+}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
