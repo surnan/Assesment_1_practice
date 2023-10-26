97fa127a1640224f63d494800490b8c7584567bb
surnan <suresh_nandlall@outlook.com>
2023-10-26 15:25:01 -0400

    Implemented: First Character & # checks

--- aa01-additional-practice/problems/04-valid-hex-code.js
+++ aa01-additional-practice/problems/04-valid-hex-code.js
@@ -14,15 +14,24 @@
 
 */
 
-console.log(validHexCode("#ABCDEF")); // prints true;
-// console.log(validHexCode("#88ef29")); // prints true;
-// console.log(validHexCode("943234")); // prints false;
-// console.log(validHexCode("#222!E4")); // prints false;
-// console.log(validHexCode("#22222")); // prints false;
+validHexCode("#ABCDEF") // prints true;
+validHexCode("#88ef29") // prints true;
+validHexCode("943234") // prints false;
+validHexCode("#222!E4") // prints false;
+validHexCode("#22222") // prints false;
 
 function validHexCode(string) {
-    // Your code here
+    //begin with # and exactly 6 characters
 
+
+    if ((string.length != 7) || string[0] != '#'){
+        // if ((string.length != 7)){
+            console.log('string.length - ', string.length, ' --string[0] - ', string[0], false);
+        return false;
+    }
+
+
+    console.log('string.length - ', string.length, ' --string[0] - ', string[0], true);
     return('---');
 }
 
